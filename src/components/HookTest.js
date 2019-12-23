5844d1b93ece89e2d1cce62c4a9202b5136fc83c
sunshineqt <17855847521@163.com>
2019-12-23 20:46:31 +0800

    redux--react-redux-react-router

--- src/components/HookTest.js
+++ src/components/HookTest.js
@@ -1,17 +1,27 @@
 // Hooks
 import React, { useState, useEffect, useReducer, useContext } from "react";
 import {connect} from 'react-redux';
-import {loadingStart, loadingEnd, init, asyncFetch} from '../store';
+// import {loadingStart, loadingEnd, init, asyncFetch} from '../store';
+import {BrowserRouter, Link, Route, Redirect, Switch} from 'react-router-dom';
+import {login} from '../store/user.redux';
+import {asyncFetch} from '../store/fruit.redux';
+
 
 function FruitList({ fruits, setFruit }) {
   return (
-    // {
-    fruits.map(f => (
-      <li key={f} onClick={() => setFruit(f)}>
-        {f}
-      </li>
-    ))
-    // }
+    <div>
+      <ul>
+        {
+          fruits.map(f => (
+            <li key={f} onClick={() => setFruit(f)}>
+              <Link to={`/list/detail/${f}`}>{f}</Link>
+            </li>
+          ))
+        }
+      </ul>
+      {/* 嵌套路由 */}
+      <Route path="/list/detail/:fruit" component={Detail}></Route>
+    </div>
   );
 }
 
@@ -37,12 +47,69 @@ const FruitAdd = connect()(function({dispatch}){
     );
   })
 
+function Detail({match, history, location}){
+  console.log(match, history, location);
+  return (
+    <div>
+        <h3>{match.params.fruit}的详情</h3>
+        <p>...</p>
+        <div>
+          <button onClick={history.goBack}>返回</button>
+        </div>
+    </div>
+   
+  )
+}
+// 创建高阶组件包装Route组件使其可以验证用户登录态
+const PrivateRoute = connect(state => ({
+  isLogin:state.user.isLogin
+}), {login})(function PrivateRoute({component:Component, isLogin, ...rest}){
+  // 结构props为component和rest
+  // rest为传递给Route丢属性
+  return (
+    <Route {...rest} render={
+      // 执行登录判断逻辑从而动态生成组件
+      props => 
+        isLogin ? (
+          <Component {...props} />
+        ):(
+          <Redirect to={{
+            pathname:'/login',
+            state:{redirect:props.location.pathname}
+            }} />
+        )
+    }>
+
+    </Route>
+  )
+})
+const Login = connect(
+  state => ({
+    isLogin:state.user.isLogin
+  }),
+  {login}
+)(function Login({location, isLogin, login}){
+  const redirect = location.state.redirect || '';
+  if(isLogin) return <Redirect to={redirect} />
+  return (
+    <div>
+      <p>用户登录</p>
+      <hr />
+      <button onClick={login}>登录</button>
+    </div>
+  )
+
+})
+
+
+
 const mapStateToProps = state => ({
-  fruits: state.list,
-  loading: state.loading
+  fruits: state.fruit.list,
+  loading: state.fruit.loading
 })
 const mapDispatchToProps = {
-  loadingStart, loadingEnd, init, asyncFetch
+  // loadingStart, loadingEnd, init, 
+  asyncFetch
 }
 
 function HookTest({fruits, loading, loadingStart, loadingEnd, init, asyncFetch}) {
@@ -73,15 +140,33 @@ function HookTest({fruits, loading, loadingStart, loadingEnd, init, asyncFetch})
   }, [fruit]);
 
   return (
-      <div>
-        <p>{fruit === "" ? "清选择喜爱的水果" : `您选择的是${fruit}`}</p>
+      <BrowserRouter>
+        <nav>
+          <Link to="/">水果列表</Link>
+          <Link to="/add">添加水果</Link>
+        </nav>
+        <div>
+        {/* exact */}
+          <Switch>
+            <Route path="/list" render={() => (
+              loading?(<div>数据加载中......</div>):(<FruitList fruits={fruits} setFruit={setFruit} />)
+            )}></Route>
+            {/* <Route path="/add" component={FruitAdd}></Route> */}
+            <PrivateRoute path="/add" component={FruitAdd} />
+            <Route path="/login" component={Login} />
+            <Route component={()=><h3>页面跑丢了</h3>}></Route>
+            {/* <Route path="/detail/:fruit" component={Detail}></Route> */}
+            {/* <Redirect></Redirect> */}
+          </Switch>
+        </div>
+        {/* <p>{fruit === "" ? "清选择喜爱的水果" : `您选择的是${fruit}`}</p> */}
         {/* <FruitAdd onAddFruit={pname => setFruits([...fruits, pname])} /> */}
         {/* <FruitAdd
           onAddFruit={pname => dispatch({ type: "add", payload: pname })}
-        /> */}
-        <FruitAdd />
-        <FruitList fruits={fruits} setFruit={setFruit} />
-      </div>
+        /> 
+        <FruitAdd />*/}
+        {/* <FruitList fruits={fruits} setFruit={setFruit} /> */}
+      </BrowserRouter>
   );
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,55 +1,14 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware,combineReducers} from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import fruitReducer from './fruit.redux';
+import user from './user.redux';
 
-export const init = (payload) => ({
-    type:'init',
-    payload
-})
-
-export const add = (payload) => ({
-    type:'add',
-    payload
-})
-
-export const loadingStart = () => ({
-    type:'loading_start'
-})
-
-export const loadingEnd = () => ({
-    type:'loading_end'
-})
-
-// 异步操作 借助中间件redux-thunk支持异步
-export const asyncFetch = payload => {
-    return dispatch => {
-        dispatch({type:'loading_start'});
-        setTimeout(() => {
-            dispatch({type:'loading_end'});
-            dispatch({type:'init', payload:['草莓', '香蕉']})
-        }, 1000)
-    }
-}
-
-function fruitReducer(state={
-    list:[],
-    loading:false
-}, action){
-    switch(action.type){
-        case 'init':
-            return { ...state, list:action.payload};
-        case 'add':
-            return { ...state, list:[...state.list, action.payload]};
-        case 'loading_start':
-            return { ...state, loading:true};
-        case 'loading_end':
-            return { ...state, loading:false};
-        default:
-            return state;                
-    }
-}
 
 // createStore参数二是中间件函数
-const store = createStore(fruitReducer, applyMiddleware(logger, thunk))
+const store = createStore(
+    combineReducers({fruit:fruitReducer, user}), 
+    applyMiddleware(logger, thunk)
+    )
 
 export default store;
